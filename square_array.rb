b1fae36105412992173a16812c6cf08c72eed8e0
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-13 20:57:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  
+  squared_array = []
+  
+  array.each do |number|
+    number *= number
+    squared_array << number
+  end
+  
+  squared_array
 end
